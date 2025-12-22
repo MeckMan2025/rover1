e915d671429d5bbdef8eef922138483fc90d7842
Andrew Meckley <andrew.meckley1981@gmail.com>
2025-12-22 16:23:05 -0600

    fix: remove unused SetRemap import causing crash

--- rover1_bringup/launch/gps.launch.py
+++ rover1_bringup/launch/gps.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction, SetRemap
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
